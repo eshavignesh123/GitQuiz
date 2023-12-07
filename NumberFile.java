5372db83aedc8d8d2e68a15d5e7986aae25fb85e
Esha Vigneswaran <30001335@sbstudents.org>
2023-12-07 11:56:10 -0500

    Created main java class

--- NumberFile.java
+++ NumberFile.java
@@ -2,3 +2,22 @@
 interface interfaceNumberFile {
     int addNumbers(int a, int b);
 }
+
+public class NumberFile {
+    public static void main(String[] args) {
+        // Lambda expression
+
+        interfaceNumberFile add = (int a, int b) -> {
+            int sum = 0;
+            for (int i = a; i <= b; i++) {
+
+                sum += i;
+
+            }
+
+            return sum;
+        };
+        System.out.println("Sum = " + add.addNumbers(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+    }
+
+}
